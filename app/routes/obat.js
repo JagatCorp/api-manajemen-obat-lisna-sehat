7033b78487fb18e418a6f3cd50b31cfdace9b175
anggagumilang212 <anggagumilang212@gmail.com>
2024-04-27 08:44:42 +0700

    route update obat

--- app/routes/obat.js
+++ app/routes/obat.js
@@ -1,31 +1,31 @@
 module.exports = (app) => {
-    const obat = require("../controllers/obatController");
-    const upl_obat = require('../middleware/obat');
+  const obat = require("../controllers/obatController");
+  const upl_obat = require("../middleware/obat");
 
-    var router = require("express").Router();
-    // Route for image upload
-    router.post("/", upl_obat.single("gambar_obat"), obat.create);
-    // Create a new Tutorial
-    router.post("/", obat.create);
-  
-    // Retrieve all Tutorials
-    router.get("/", obat.findAll);
-  
-    // Retrieve all published obat
-    // router.get("/published", order.findAllPublished);
-  
-    // Retrieve a single Tutorial with id
-    router.get("/:id", obat.findOne);
-  
-    // Update a Tutorial with id
-    router.post("/:id", obat.update);
-  
-    // Delete a Tutorial with id
-    router.delete("/:id", obat.delete);
-  
-    // Delete all obat
-    router.delete("/", obat.deleteAll);
-  
-    app.use("/api/obat", router);
-  };
-  
+  var router = require("express").Router();
+  // Route for image upload
+  router.post("/", upl_obat.single("gambar_obat"), obat.create);
+  // Create a new Tutorial
+  router.post("/", obat.create);
+
+  // Retrieve all Tutorials
+  router.get("/", obat.findAll);
+
+  // Retrieve all published obat
+  // router.get("/published", order.findAllPublished);
+
+  // Retrieve a single Tutorial with id
+  router.get("/:id", obat.findOne);
+
+  // Update a Tutorial with id
+  // router.put("/:id", obat.update);
+  router.put("/:id", upl_obat.single("gambar_obat"), obat.update);
+
+  // Delete a Tutorial with id
+  router.delete("/:id", obat.delete);
+
+  // Delete all obat
+  router.delete("/", obat.deleteAll);
+
+  app.use("/api/obat", router);
+};
