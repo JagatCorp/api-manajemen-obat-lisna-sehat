cd83311f1f7dfa776b4fb6c3277ad3ad2585300c
anggagumilang212 <anggagumilang212@gmail.com>
2024-05-21 11:23:13 +0700

    restore

--- app/controllers/transaksidistributorController.js
+++ app/controllers/transaksidistributorController.js
@@ -7,6 +7,50 @@ const JSONAPISerializer = require("jsonapi-serializer").Serializer;
 const { Op, where } = require("sequelize");
 
 // Create and Save a new transaksidistributor
+// exports.create = async (req, res) => {
+//   try {
+//     // Validate request
+//     if (
+//       !req.body.jml_barang ||
+//       !req.body.harga ||
+//       !req.body.status ||
+//       !req.body.nama_pembeli ||
+//       !req.body.barang_distributorId
+//     ) {
+//       return res.status(400).send({ message: "Data is required!" });
+//     }
+
+//     // Find Barangdistributor by Barangdistributor_id
+//     const dataBarangdistributor = await Barangdistributor.findByPk(
+//       req.body.barang_distributorId
+//     );
+//     if (!dataBarangdistributor) {
+//       return res.status(404).send({ message: "Barangdistributor not found!" });
+//     }
+
+//     // Create transaksidistributor object with Barangdistributor_id
+//     const transaksidistributor = {
+//       jml_barang: req.body.jml_barang,
+//       harga: req.body.harga,
+//       status: req.body.status,
+//       barang_distributorId: req.body.barang_distributorId,
+//       nama_pembeli: req.body.nama_pembeli,
+//       // Assign Barangdistributor_id from request body
+//     };
+
+//     // Save transaksidistributor to the database
+//     const createdTransaksidistributor = await Transaksidistributor.create(
+//       transaksidistributor
+//     );
+//     res.send(createdTransaksidistributor);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).send({
+//       message: error.message || "Error creating transaksidistributor.",
+//     });
+//   }
+// };
+
 exports.create = async (req, res) => {
   try {
     // Validate request
@@ -28,6 +72,26 @@ exports.create = async (req, res) => {
       return res.status(404).send({ message: "Barangdistributor not found!" });
     }
 
+    // Check if there's enough stock
+    if (
+      parseInt(dataBarangdistributor.satuan_stok_barang) <
+        parseInt(req.body.jml_barang) &&
+      req.body.status == "Keluar"
+    ) {
+      return res.status(500).send({ message: "Not enough stock available!" });
+    }
+
+    if (req.body.status == "Keluar") {
+      // Reduce stock in Barangdistributor
+      dataBarangdistributor.satuan_stok_barang -= parseInt(req.body.jml_barang);
+    } else {
+      // Increase stock in Barangdistributor
+      dataBarangdistributor.satuan_stok_barang += parseInt(req.body.jml_barang);
+    }
+
+    dataBarangdistributor.harga_satuan_barang = req.body.harga;
+    await dataBarangdistributor.save();
+
     // Create transaksidistributor object with Barangdistributor_id
     const transaksidistributor = {
       jml_barang: req.body.jml_barang,
@@ -35,13 +99,13 @@ exports.create = async (req, res) => {
       status: req.body.status,
       barang_distributorId: req.body.barang_distributorId,
       nama_pembeli: req.body.nama_pembeli,
-      // Assign Barangdistributor_id from request body
     };
 
     // Save transaksidistributor to the database
     const createdTransaksidistributor = await Transaksidistributor.create(
       transaksidistributor
     );
+
     res.send(createdTransaksidistributor);
   } catch (error) {
     console.error(error);
@@ -86,6 +150,74 @@ exports.findAll = async (req, res) => {
       searchQuery
     );
     const totalCount = await Transaksidistributor.count();
+
+    const totalPages = Math.ceil(totalCount / pageSize);
+
+    res.send({
+      data: transaksidistributor,
+      currentPage: page,
+      totalPages: totalPages,
+      pageSize: pageSize,
+      totalCount: totalCount,
+    });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(500)
+      .send({ message: "Error retrieving transaksidistributors." });
+  }
+};
+
+exports.deleteList = async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.pageSize) || 10;
+    const offset = (page - 1) * pageSize;
+
+    const keyword = req.query.keyword || "";
+
+    // Constructing the search query
+    const searchQuery = {
+      where: {
+        deletedAt: {
+          [Op.ne]: null, // Only select records with deletedAt not null
+        },
+        [Op.or]: [
+          { jml_barang: { [Op.like]: `%${keyword}%` } },
+          { harga: { [Op.like]: `%${keyword}%` } },
+          { status: { [Op.like]: `%${keyword}%` } },
+          { nama_pembeli: { [Op.like]: `%${keyword}%` } },
+        ],
+      },
+      paranoid: false,
+      limit: pageSize,
+      offset: offset,
+      include: [
+        {
+          model: Barangdistributor,
+          attributes: ["nama_barang", "urlGambar"],
+        },
+      ],
+    };
+
+    const transaksidistributor = await Transaksidistributor.findAll(
+      searchQuery
+    );
+
+    // Count total soft deleted records matching the search criteria
+    const totalCount = await Transaksidistributor.count({
+      where: {
+        deletedAt: {
+          [Op.ne]: null, // Count only records with deletedAt not null
+        },
+        [Op.or]: [
+          { jml_barang: { [Op.like]: `%${keyword}%` } },
+          { harga: { [Op.like]: `%${keyword}%` } },
+          { status: { [Op.like]: `%${keyword}%` } },
+          { nama_pembeli: { [Op.like]: `%${keyword}%` } },
+        ],
+      },
+    });
 
     const totalPages = Math.ceil(totalCount / pageSize);
 
@@ -160,12 +292,165 @@ exports.update = async (req, res) => {
     });
 };
 
+exports.restore = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    // Find the soft deleted transaction
+    const transaksidistributor = await Transaksidistributor.findOne({
+      where: {
+        id: id,
+      },
+      paranoid: false
+    });
+
+    
+    const barangdistributor = await Barangdistributor.findOne({
+      where: {
+        id: transaksidistributor.barang_distributorId,
+      }
+    })
+
+    // return res.status(500).json({
+    //   message: barangdistributor
+    // })
+
+    // Check if there's enough stock
+    if (
+      parseInt(barangdistributor.satuan_stok_barang) <
+        parseInt(transaksidistributor.jml_barang) &&
+        transaksidistributor.status == "Keluar"
+    ) {
+      return res.status(500).send({ message: "Not enough stock available!" });
+    }
+
+    if (transaksidistributor.status == "Keluar") {
+      // Reduce stock in Barangdistributor
+      barangdistributor.satuan_stok_barang -= parseInt(transaksidistributor.jml_barang);
+    } else {
+      // Increase stock in Barangdistributor
+      barangdistributor.satuan_stok_barang += parseInt(transaksidistributor.jml_barang);
+    }
+
+    await barangdistributor.save();
+
+    if (!transaksidistributor) {
+      return res.status(404).json({
+        message: `Cannot find soft deleted transaksidistributor with id=${id}.`,
+      });
+    }
+
+    // Restore the transaction by setting deletedAt to null
+    // transaksidistributor.deletedAt = null;
+    await transaksidistributor.restore();
+
+    return res.status(200).json({
+      message: "Transaksidistributor was restored successfully!",
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: "Error restoring transaksidistributor with id=" + id,
+    });
+  }
+};
+
 // Delete a transaksidistributor with the specified id in the request
-exports.delete = (req, res) => {
+// exports.delete = (req, res) => {
+//   const id = req.params.id;
+//   const currentDate = new Date().toISOString();
+
+//   Transaksidistributor.update(
+//     { deletedAt: currentDate },
+//     { where: { id: id } }
+//   )
+//     .then((num) => {
+//       if (num == 1) {
+//         res.send({
+//           message: "transaksidistributor was soft deleted successfully!",
+//         });
+//       } else {
+//         res.send({
+//           message: `Cannot soft delete transaksidistributor with id=${id}. Maybe transaksidistributor was not found!`,
+//         });
+//       }
+//     })
+//     .catch((err) => {
+//       res.status(500).send({
+//         message: "Could not soft delete transaksidistributor with id=" + id,
+//       });
+//     });
+// };
+
+exports.delete = async (req, res) => {
+  const id = req.params.id;
+  const currentDate = new Date().toISOString();
+
+  try {
+    // Find the transaction to get the jml_barang and barang_distributorId
+    const transaksidistributor = await Transaksidistributor.findByPk(id);
+    if (!transaksidistributor) {
+      return res.status(404).send({
+        message: `Cannot soft delete transaksidistributor with id=${id}. Maybe transaksidistributor was not found!`,
+      });
+    }
+
+    // Find the associated Barangdistributor
+    const dataBarangdistributor = await Barangdistributor.findByPk(
+      transaksidistributor.barang_distributorId
+    );
+    if (!dataBarangdistributor) {
+      return res.status(404).send({
+        message: `Cannot find Barangdistributor with id=${transaksidistributor.barang_distributorId}`,
+      });
+    }
+
+    // Add the jml_barang back to satuan_stok_barang
+    // return res.status(400).send({ message: transaksidistributor.status == 'Keluar' });
+    if (transaksidistributor.status == "Keluar") {
+      // Increase stock in Barangdistributor
+      dataBarangdistributor.satuan_stok_barang += parseInt(
+        transaksidistributor.jml_barang
+      );
+    } else {
+      if (
+        parseInt(dataBarangdistributor.satuan_stok_barang) -
+          parseInt(transaksidistributor.jml_barang) <
+        0
+      ) {
+        return res.status(400).send({ message: "Not enough stock available!" });
+      } else {
+        // Reduce stock in Barangdistributor
+        dataBarangdistributor.satuan_stok_barang -= parseInt(
+          transaksidistributor.jml_barang
+        );
+      }
+    }
+    await dataBarangdistributor.save();
+
+    // Update the deletedAt field to soft delete the transaction
+    await Transaksidistributor.update(
+      { deletedAt: currentDate },
+      { where: { id: id } }
+    );
+
+    res.send({
+      message: "transaksidistributor was soft deleted successfully!",
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send({
+      message: "Could not soft delete transaksidistributor with id=" + id,
+    });
+  }
+};
+// delete permanent
+exports.deletePermanent = (req, res) => {
   const id = req.params.id;
 
   Transaksidistributor.destroy({
     where: { id: id },
+    force: true, // Menghapus permanen
   })
     .then((num) => {
       if (num == 1) {
@@ -184,7 +469,6 @@ exports.delete = (req, res) => {
       });
     });
 };
-
 // Delete all transaksidistributors from the database.
 exports.deleteAll = (req, res) => {
   Transaksidistributor.destroy({
--- app/routes/transaksidistributor.js
+++ app/routes/transaksidistributor.js
@@ -1,29 +1,33 @@
 module.exports = (app) => {
-    const transaksidistributor = require("../controllers/transaksidistributorController");
-  
-    var router = require("express").Router();
-  
-    // Create a new Tutorial
-    router.post("/", transaksidistributor.create);
-  
-    // Retrieve all Tutorials
-    router.get("/", transaksidistributor.findAll);
-  
-    // Retrieve all published transaksidistributor
-    // router.get("/published", transaksidistributor.findAllPublished);
-  
-    // Retrieve a single Tutorial with id
-    router.get("/:id", transaksidistributor.findOne);
-  
-    // Update a Tutorial with id
-    router.put("/:id", transaksidistributor.update);
-  
-    // Delete a Tutorial with id
-    router.delete("/:id", transaksidistributor.delete);
-  
-    // Delete all transaksidistributor
-    router.delete("/", transaksidistributor.deleteAll);
-  
-    app.use("/api/transaksidistributor", router);
-  };
-  
+  const transaksidistributor = require("../controllers/transaksidistributorController");
+
+  var router = require("express").Router();
+
+  // Create a new Tutorial
+  router.post("/", transaksidistributor.create);
+
+  // Retrieve all Tutorials
+  router.get("/", transaksidistributor.findAll);
+
+  router.get("/deletelist", transaksidistributor.deleteList);
+
+  // Retrieve all published transaksidistributor
+  // router.get("/published", transaksidistributor.findAllPublished);
+
+  // Retrieve a single Tutorial with id
+  router.get("/:id", transaksidistributor.findOne);
+
+  // Update a Tutorial with id
+  router.put("/:id", transaksidistributor.update);
+  router.put("/restore/:id", transaksidistributor.restore);
+
+  // Delete a Tutorial with id
+  router.delete("/:id", transaksidistributor.delete);
+  // Delete a Tutorial with id
+  router.delete("/permanent/:id", transaksidistributor.deletePermanent);
+
+  // Delete all transaksidistributor
+  router.delete("/", transaksidistributor.deleteAll);
+
+  app.use("/api/transaksidistributor", router);
+};
