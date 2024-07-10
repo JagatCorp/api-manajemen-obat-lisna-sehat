8173b615084cafd9895652dd8362382e487b60cd
RamadhanRendi18 <ramadhanprogramrendi01@gmail.com>
2024-07-10 15:23:41 +0700

    penjual pembuat

--- app/routes/penjualpembuat.js
+++ app/routes/penjualpembuat.js
@@ -1,29 +1,25 @@
-module.exports = (app) => {
-    const penjualpembuat = require("../controllers/penjualpembuatController");
-  
-    var router = require("express").Router();
-  
-    // Create a new Tutorial
-    router.post("/", penjualpembuat.create);
-  
-    // Retrieve all Tutorials
-    router.get("/", penjualpembuat.findAll);
-  
-    // Retrieve all published penjualpembuat
-    // router.get("/published", order.findAllPublished);
-  
-    // Retrieve a single Tutorial with id
-    router.get("/:id", penjualpembuat.findOne);
-  
-    // Update a Tutorial with id
-    router.put("/:id", penjualpembuat.update);
-  
-    // Delete a Tutorial with id
-    router.delete("/:id", penjualpembuat.delete);
-  
-    // Delete all penjualpembuat
-    router.delete("/", penjualpembuat.deleteAll);
-  
-    app.use("/api/penjualpembuat", router);
-  };
-  
+module.exports = app => {
+  const penjualpembuat = require("../controllers/penjualpembuatController");
+  const express = require('express');
+  const router = express.Router();
+
+  // Create a new Tutorial
+  router.post("/", penjualpembuat.create);
+
+  // Retrieve all Tutorials
+  router.get("/", penjualpembuat.findAll);
+
+  // Retrieve a single Tutorial with id
+  router.get("/:id", penjualpembuat.findOne);
+
+  // Update a Tutorial with id
+  router.put("/:id", penjualpembuat.update);
+
+  // Delete a Tutorial with id
+  router.delete("/:id", penjualpembuat.delete);
+
+  // Delete all penjualpembuat
+  router.delete("/", penjualpembuat.deleteAll);
+
+  app.use('/api/penjualpembuat', router);
+};
--- server.js
+++ server.js
@@ -56,6 +56,7 @@ require("./app/routes/transaksiobatmasuk")(app);
 require("./app/routes/principle")(app);
 require("./app/routes/pembelidistributor")(app);
 require("./app/routes/transaksidistributor")(app);
+require("./app/routes/penjualpembuat")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
